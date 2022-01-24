6aaed3bc1c418f1a1da15a2b72210c9ff0d977e3
Pranav Ajay <86167689+MoeZilla@users.noreply.github.com>
2022-01-24 14:45:38 +0530

    Update __init__.py

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -26,33 +26,6 @@ from config import DEV, LOGS
 # from .. import LOGS
 
 
-class Sylviorus:
-
-    def __init__(self, token) -> None:
-        self.devs = DEVS
-        self.logs = LOGS
-        self.base = "https://sylviorus-api.up.railway.app/"
-        self.admin_token = token
-
-    def check(self, user: int) -> dict:
-        data = get(f"{self.base}user/{user}").json()
-        return data
-
-    def ban(self, user: int, reason: str, enforcer: int) -> str:
-        data = {
-            "user": user,
-            "reason": reason,
-            "enforcer": enforcer,
-            "admin_token": self.admin_token
-        }
-        res = post(f"{self.base}ban", json=data)
-        return res.text
-
-    def unban(self, user):
-        data = {"user": user, "admin_token": self.admin_token}
-        res = post(f"{self.base}unban", json=data)
-        return res.text
-
 
 def check_dev(user: int):
     if user in DEVS:
