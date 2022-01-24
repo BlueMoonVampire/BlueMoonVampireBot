ba5afd185d4b942285755f1bfce58b19a975a649
Pranav Ajay <86167689+MoeZilla@users.noreply.github.com>
2022-01-24 14:41:44 +0530

    Update start.py

--- app/plugins/start.py
+++ app/plugins/start.py
@@ -35,8 +35,4 @@ Hi, Welcome {}
 
 @bot.on_message(filters.command(["start"], ['/', ".", "?"]))
 async def start(client, message):
-    buttons = [[
-        InlineKeyboardButton("Support", url="https://t.me/Sylviorus_support"),
-    ]]
-    await message.reply_text(sylmessage.format(message.from_user.first_name),
-                             reply_markup=InlineKeyboardMarkup(buttons))
+    await message.reply_text(sylmessage.format(message.from_user.first_name))
