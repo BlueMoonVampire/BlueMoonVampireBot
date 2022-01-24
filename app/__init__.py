f1771bd03c2515fea96f8958381be81bf01d4d3a
Pranav Ajay <86167689+MoeZilla@users.noreply.github.com>
2022-01-24 14:25:39 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -22,16 +22,15 @@ SOFTWARE.
 """
 
 from pyrogram import Client
-from config import API_ID, API_HASH, BOT_TOKEN, SYLTOKEN
+from config import API_ID, API_HASH, SESSION
 from .utils import Sylviorus
 from .db import LocalDb
 
-bot = Client("Sylviorus",
-             api_id=API_ID,
-             api_hash=API_HASH,
-             bot_token=BOT_TOKEN,
-             plugins=dict(root="{}/plugins".format(__name__)))
-
+bot = Client(
+      session_name=SESSION,
+      api_id=API_ID,
+      api_hash=API_HASH,
+      plugins=dict(root="{}/plugins".format(__name__))
+)
 
 ldb = LocalDb("reasons")
-SYL = Sylviorus(SYLTOKEN)
