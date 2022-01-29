d0b93bac1c44ead72cbf4e3d09327b97f0a4ad7c
Pranav Ajay <86167689+MoeZilla@users.noreply.github.com>
2022-01-29 14:56:36 +0530

    Update alert.py

--- app/plugins/alert.py
+++ app/plugins/alert.py
@@ -1,34 +1,15 @@
-"""
-MIT License
-Copyright (C) 2021-2022, NkSama
-Copyright (C) 2021-2022 Moezilla
-Copyright (c) 2021, Sylviorus, <https://github.com/Sylviorus/BlueMoonVampireBot>
-This file is part of @BlueMoonVampireBot (Antispam Telegram Bot)
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
 
 from pyrogram import filters
-from .. import bot, SYL
+from vanitas import User as vanitas
+from time import time
+from pyrogram.types import ChatPermissions
 from requests import get
 from ..utils import DEVS
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.types import Message
-from app.plugins.token import db
+from app.plugins.statusflix import db
+
+v = vanitas()
 
 
 def is_blacklisted(user):
@@ -53,37 +34,92 @@ def call_back_filter(data):
 
 
 @bot.on_callback_query(call_back_filter("kick"))
-def ban_callback(_, query):
+def kick_callback(_, query):
     user = query.data.split(":")[2]
     if is_admin(query.message.chat.id,
                 query.from_user.id) and query.data.split(":")[1] == "kick":
         bot.ban_chat_member(query.message.chat.id, user)
         bot.unban_chat_member(query.message.chat.id, user)
         query.answer('Kicked!')
-        query.message.reply(
-            f'Banned [{user}](tg://user?id={user})\n From [{query.from_user.id}](tg://user?id={query.from_user.id})',
+        query.message.edit(
+            f'Kick User [{user}](tg://user?id={user})\n Admin User [{query.from_user.id}](tg://user?id={query.from_user.id})',
             parse_mode='markdown')
     else:
         message.reply('You are not admin!')
 
 
+@bot.on_callback_query(call_back_filter("ban"))
+def ban_callback(_, query):
+    user = query.data.split(":")[2]
+    if is_admin(query.message.chat.id,
+                query.from_user.id) and query.data.split(":")[1] == "ban":
+        bot.ban_chat_member(query.message.chat.id, user)
+        query.answer('Banned')
+        query.message.edit(
+            f'Banned User [{user}](tg://user?id={user})\n Admin User [{query.from_user.id}](tg://user?id={query.from_user.id})',
+            parse_mode='markdown')
+    else:
+        message.reply('You are not admin!')
+
+
+@bot.on_callback_query(call_back_filter("mute"))
+def mute_callback(_, query):
+    user = query.data.split(":")[2]
+    if is_admin(query.message.chat.id,
+                query.from_user.id) and query.data.split(":")[1] == "mute":
+        bot.restrict_chat_member(
+            query.message.chat.id,
+            user,
+            ChatPermissions(can_send_messages=False),
+        )
+        query.answer('Muted!')
+        query.message.edit(
+            f'Muted User [{user}](tg://user?id={user})\n Admin User [{query.from_user.id}](tg://user?id={query.from_user.id})',
+            parse_mode='markdown')
+    else:
+        message.reply('You are not admin!')
+
+
+@bot.on_message(
+    filters.command("alertmode", prefixes=["/", ".", "?", "-"])
+    & ~filters.private)
+async def alertmode(_, message):
+    if is_admin(message.user.id, message.chat.id):
+        return
+    is_vanitas = db.is_vanitas(message.chat.id)
+    if not is_vanitas:
+        db.set_vanitas(message.chat.id)
+        await message.reply_text("Alert Mode Enable")
+    else:
+        db.rm_vanitas(message.chat.id)
+        await message.reply_text("Alert Mode Disable")
+
+
 @bot.on_message(filters.new_chat_members)
 def alert(_, m: Message):
     user = m.from_user.id
+    is_vanitas = db.is_vanitas(m.chat.id)
+    if not is_vanitas:
+        return
     if is_blacklisted(user):
-        x = SYL.check(user)
-        bot.send_message(m.chat.id,
-                         f"""
+        x = v.get_info(user)
+        bot.send_message(
+            m.chat.id,
+            f"""
 #ALERT
 **This User Is Blacklisted**
 **USER** : [{user}](tg://user?id={user})
-**REASON** : {x["reason"]}
-**ENFORCER** : [{x["enforcer"]}](tg://user?id={x["enforcer"]})""",
-                         reply_markup=InlineKeyboardMarkup([
-                             [
-                                 InlineKeyboardButton(
-                                     "Kick", callback_data=f"kick:kick:{user}")
-                             ],
-                         ]))
+**REASON** : {x.reason}
+**ENFORCER** : [{x.enforcer}](tg://user?id={x.enforcer})""",
+            reply_markup=InlineKeyboardMarkup([
+                [
+                    InlineKeyboardButton("Ban",
+                                         callback_data=f"ban:ban:{user}"),
+                    InlineKeyboardButton("Kick",
+                                         callback_data=f"kick:kick:{user}"),
+                    InlineKeyboardButton("Mute",
+                                         callback_data=f"mute:mute:{user}")
+                ],
+            ]))
     else:
         pass
