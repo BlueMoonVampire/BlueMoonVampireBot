5c40d9fa4491ef201026a6a9923612ad1b154ac9
Pranav Ajay <86167689+MoeZilla@users.noreply.github.com>
2022-02-04 10:09:12 +0530

    Update alert.py

--- app/plugins/alert.py
+++ app/plugins/alert.py
@@ -1,20 +1,66 @@
+import os
+import random
+import aiofiles
+import aiohttp
+from os import path
+from PIL import Image, ImageDraw, ImageFont
 from pyrogram import filters
 from vanitas import User as vanitas
-from .. import bot
+from .. import bot, SYL
 from time import time
 from pyrogram.types import ChatPermissions
 from requests import get
 from ..utils import DEVS
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.types import Message
-from app.plugins.status import db
+from app.plugins.token import db
+
+
+
+async def blacklistimage(enforcer, title, user, reason, vanitasimage):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(vanitasimage) as resp:
+            if resp.status == 200:
+                f = await aiofiles.open("report.png", mode="wb")
+                await f.write(await resp.read())
+                await f.close()
+
+    image1 = Image.open("./report.png")
+    image2 = image1.convert("RGBA")
+    Image.alpha_composite(image2).save("save.png")
+    i = Image.open("save.png")
+    draw = ImageDraw.Draw(i)
+    font = ImageFont.truetype("app/fonts.otf", 32)
+    draw.text(
+        (205, 590),
+        f"USER: {user} | {title}",
+        fill="white",
+        stroke_width=1,
+        stroke_fill="black",
+        font=font,
+    )
+    draw.text(
+        (205, 630),
+        f"REASON: {reason}",
+        fill="white",
+        stroke_width=1,
+        stroke_fill="black",
+        font=font,
+    )
+    draw.text(
+        (205, 670),
+        f"ENFORCER: {enforcer}",
+        fill="white",
+        stroke_width=1,
+        stroke_fill="black",
+        font=font,
+    )
+    i.save("vanitas.png")
+    os.remove("save.png")
+    os.remove("report.png")
+
 
 v = vanitas()
-
-
-def is_blacklisted(user):
-    res = get("https://vanitas-api.up.railway.app/user/" + str(user)).json()
-    return True if res['blacklisted'] else False
 
 
 def is_admin(group_id: int, user_id: int):
@@ -65,8 +111,8 @@ def ban_callback(_, query):
 @bot.on_callback_query(call_back_filter("mute"))
 def mute_callback(_, query):
     user = query.data.split(":")[2]
-    if is_admin(query.message.chat.id,
-                query.from_user.id) and query.data.split(":")[1] == "mute":
+    if is_admin(query.from_user.id,
+                query.message.chat.id) and query.data.split(":")[1] == "mute":
         bot.restrict_chat_member(
             query.message.chat.id,
             user,
@@ -80,37 +126,21 @@ def mute_callback(_, query):
         message.reply('You are not admin!')
 
 
-@bot.on_message(
-    filters.command("alertmode", prefixes=["/", ".", "?", "-"])
-    & ~filters.private)
-async def alertmode(_, message):
-    if is_admin(message.chat.id, message.from_user.id):
-        return
-    is_vanitas = db.is_vanitas(message.chat.id)
-    if not is_vanitas:
-        db.set_vanitas(message.chat.id)
-        await message.reply_text("Alert Mode Enable")
-    else:
-        db.rm_vanitas(message.chat.id)
-        await message.reply_text("Alert Mode Disable")
-
-
 @bot.on_message(filters.new_chat_members)
-def alert(_, m: Message):
+async def alert(_, m: Message):
     user = m.from_user.id
-    is_vanitas = db.is_vanitas(m.chat.id)
-    if not is_vanitas:
-        return
-    if is_blacklisted(user):
-        x = v.get_info(user)
-        bot.send_message(
-            m.chat.id,
-            f"""
-#ALERT
-**This User Is Blacklisted**
-**USER** : [{user}](tg://user?id={user})
-**REASON** : {x.reason}
-**ENFORCER** : [{x.enforcer}](tg://user?id={x.enforcer})""",
+    x = v.get_info(user)
+    if x.blacklisted:
+        vanitasimage = "https://telegra.ph/file/c4bcc673963abc4fdb137.jpg"
+        user = user
+        title = m.from_user.first_name
+        reason = x.reason
+        enforcer = x.enforcer
+        await blacklistimage(enforcer, title, user, reason, vanitasimage)
+        await m.reply_photo(
+            photo="vanitas.png",
+            caption=
+            f"#ALERT\n**This User Is Blacklisted**\n**USER** : [{user}](tg://user?id={user})\n**REASON** : {x.reason}\n**ENFORCER** : [{x.enforcer}](tg://user?id={x.enforcer})",
             reply_markup=InlineKeyboardMarkup([
                 [
                     InlineKeyboardButton("Ban",
